dc2b4b8c3e7fe11257365b53030b1c20da8e04f6
John Loberger <johnloberger@gmail.com>
2020-04-28 19:13:09 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,17 +1,17 @@
 def find_min_in_nested_arrays(src)
 results = []
 row_index = 0
-  while row_index < src.count do
-    element_index = 0
-    min_string_element = Float::INFINITY
-    while element_index < src[row_index].count do
-      if src[row_index][element_index] < min_string_element
-        min_string_element = src[row_index][element_index]
+    while row_index < src.count do
+      element_index = 0
+      min_string_element = Float::INFINITY
+      while element_index < src[row_index].count do
+        if src[row_index][element_index] < min_string_element
+          min_string_element = src[row_index][element_index]
+        end
+        element_index += 1
       end
-      element_index += 1
+      results << min_string_element
+      row_index += 1
   end
-  results << min_string_element
-  row_index += 1
 end
-
 results
